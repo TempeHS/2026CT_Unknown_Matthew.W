a76e81d0a65446ff8d994510a28e73fd68e8c276
Matthew W <matthew.wu12@education.nsw.gov.au>
2026-05-14 09:40:15 +1000

    WIP: Player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float MovementSpeed: 5f;
+    private Rigidboy2D rb;
+    private Vector2 moveInput;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
